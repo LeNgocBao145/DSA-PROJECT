8e95675e1cbe1e8cd9124ba430a92054f820a1f5
LeNgocBao145 <himagito@gmail.com>
2024-06-25 20:34:20 +0700

    fix

--- cmdarg/sort.h
+++ cmdarg/sort.h
@@ -9,12 +9,12 @@ void mergeSort(int* arr, int left, int right, int& comparision);
 void quickSort(int* arr, int left, int right, int& comparision);
 
 //Thuật toán sắp xếp cocktail sort hay còn gọi là shaker sort (là bubble sort 2 chiều xuôi và ngược)
-void shakerSort(int* arr[], int n);
+void shakerSort(int* arr, int n);
 
 //Thuật toán shell sort sử dụng gap là n/2 cho mỗi bước sort để chia thành nhiều sub - arrays nhỏ qua 
 //mỗi lần chia thì gap /= 2 và sort bằng insertion sort
-void shellSort(int* arr[], int n);
+void shellSort(int* arr, int n);
 
-void radixSort(int arr[], int n);
+void radixSort(int arr, int n);
 
 

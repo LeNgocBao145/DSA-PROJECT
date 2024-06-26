d9b15a643b09cb25d771ed76602beb2ceef32c08
Huynh Nhat Huy <nhathuyth1@gmail.com>
2024-06-26 10:29:23 +0700

    Delete unwanted files

--- cmdarg/heap.h
+++
@@ -1,5 +0,0 @@
-#ifndef HEAP_SORT_H
-#define HEAP_SORT_H
-void heapify(int* arr, int n, int i, int& comparision);
-void heapSort(int* arr, int n, int& comparision);
-#endif
--- cmdarg/quick.h
+++
@@ -1,4 +0,0 @@
-#ifndef QUICK_SORT_H
-#define QUICK_SORT_H
-void quickSort(int* arr, int left, int right, int& comparision);
-#endif
